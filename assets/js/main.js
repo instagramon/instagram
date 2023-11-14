4c89e8b100b1a144705e08cb0a423f4181feaa4c
instagramon <150757545+instagramon@users.noreply.github.com>
2023-11-14 01:46:37 +0000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('.btn');
+const cor = document.querySelector('.cor');
+
+
+const digito = ()=>{
+
+var caracter = document.getElementById('entrada').value
+
+
+if(caracter.length > 5){
+btn.classList.add('cor');
+}
+}
